19fa9065775763dcafaa12ab35e7625b64d560ba
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2025-10-19 22:28:22 +0200

    eslint/prettier - fix components/form/email-form

--- src/components/forms/email-form/email-form.tsx
+++ src/components/forms/email-form/email-form.tsx
@@ -1,19 +1,22 @@
-import { Input, clx } from "@medusajs/ui"
-import { Control } from "react-hook-form"
-import { useTranslation } from "react-i18next"
-import { z } from "zod"
-import { EmailSchema } from "../../../lib/schemas"
-import { Form } from "../../common/form"
+import { Input, clx } from "@medusajs/ui";
 
-type EmailFieldValues = z.infer<typeof EmailSchema>
+import type { Control } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import type { z } from "zod";
+
+import { Form } from "@components/common/form";
+
+import type { EmailSchema } from "@lib/schemas";
+
+type EmailFieldValues = z.infer<typeof EmailSchema>;
 
 type EmailFormProps = {
-  control: Control<EmailFieldValues>
-  layout?: "grid" | "stack"
-}
+  control: Control<EmailFieldValues>;
+  layout?: "grid" | "stack";
+};
 
 export const EmailForm = ({ control, layout = "stack" }: EmailFormProps) => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <div
@@ -25,18 +28,16 @@ export const EmailForm = ({ control, layout = "stack" }: EmailFormProps) => {
       <Form.Field
         control={control}
         name="email"
-        render={({ field }) => {
-          return (
-            <Form.Item>
-              <Form.Label>{t("fields.email")}</Form.Label>
-              <Form.Control>
-                <Input {...field} />
-              </Form.Control>
-              <Form.ErrorMessage />
-            </Form.Item>
-          )
-        }}
+        render={({ field }) => (
+          <Form.Item>
+            <Form.Label>{t("fields.email")}</Form.Label>
+            <Form.Control>
+              <Input {...field} />
+            </Form.Control>
+            <Form.ErrorMessage />
+          </Form.Item>
+        )}
       />
     </div>
-  )
-}
+  );
+};
