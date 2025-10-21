06ca662f1f0199ed5da71a82a083923208079f33
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2025-10-21 10:59:18 +0200

    eslint/prettier - fix components/table/table-cells/shipping-option/shipping-price-cell

--- src/components/table/table-cells/shipping-option/shipping-price-cell/shipping-price-cell.tsx
+++ src/components/table/table-cells/shipping-option/shipping-price-cell/shipping-price-cell.tsx
@@ -1,12 +1,13 @@
-import { useTranslation } from "react-i18next"
-import { MoneyAmountCell } from "../../common/money-amount-cell"
-import { PlaceholderCell } from "../../common/placeholder-cell"
+import { useTranslation } from "react-i18next";
+
+import { MoneyAmountCell } from "@components/table/table-cells/common/money-amount-cell";
+import { PlaceholderCell } from "@components/table/table-cells/common/placeholder-cell";
 
 type ShippingPriceCellProps = {
-  isCalculated: boolean
-  price?: number | null
-  currencyCode: string
-}
+  isCalculated: boolean;
+  price?: number | null;
+  currencyCode: string;
+};
 
 export const ShippingPriceCell = ({
   price,
@@ -14,18 +15,18 @@ export const ShippingPriceCell = ({
   isCalculated,
 }: ShippingPriceCellProps) => {
   if (isCalculated || !price) {
-    return <PlaceholderCell />
+    return <PlaceholderCell />;
   }
 
-  return <MoneyAmountCell currencyCode={currencyCode} amount={price} />
-}
+  return <MoneyAmountCell currencyCode={currencyCode} amount={price} />;
+};
 
 export const ShippingPriceHeader = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <div className="flex items-center overflow-hidden">
       <span className="truncate">{t("fields.price")}</span>
     </div>
-  )
-}
+  );
+};
